f3b7b3f09bba17561e03c8818de34aaccd272d68
MatveevDaniil <dmatveev@sfsu.edu>
2024-09-20 15:32:14 -0700

    FIX: switching iterator optimized version to ikj order

--- dgemm-basic-iterator-optimization.cpp
+++ dgemm-basic-iterator-optimization.cpp
@@ -2,12 +2,9 @@ const char* dgemm_desc = "Basic implementation, three-loop dgemm with iterator o
 
 void square_dgemm(int n, double* A, double* B, double* C) 
 {
-  double dot_prod, *A_i, *C_i, *B_j, *C_ij, *A_ik, *B_kj;
+  double *A_i, *C_i, *B_k, *C_ij, *A_ik, *B_kj;
   for (A_i = A, C_i = C; A_i < A + n * n; A_i += n, C_i += n)
-    for (B_j = B, C_ij = C_i; B_j < B + n; B_j += 1, C_ij += 1) {
-      dot_prod = 0;
-      for (A_ik = A_i, B_kj = B_j; B_kj < B_j + n * n; A_ik += 1, B_kj += n)
-        dot_prod += (*A_ik) * (*B_kj);
-      *C_ij += dot_prod;
-    }
+    for (B_k = B; B_k < B + n * n; B_k += n)
+      for (C_ij = C_i, A_ik = A_i, B_kj = B_k; C_ij < C_i + n; C_ij++, A_ik++, B_kj++)
+        *C_ij += (*A_ik) * (*B_kj);
 }
