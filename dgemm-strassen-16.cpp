4e1d7c5da45e022fdae6b9c403a5b689fa234e1c
MatveevDaniil <dmatveev@sfsu.edu>
2024-09-21 13:54:14 -0700

    FIX: type problem

--- dgemm-strassen-16.cpp
+++ dgemm-strassen-16.cpp
@@ -5,7 +5,7 @@ const char* dgemm_desc = "Strassen-Winograd dgemm 16.";
 void square_dgemm(int n, double* A, double* B, double* C) 
 {
   std::vector<double> buf(n * n);
-  _C = buf.data() + 0;
+  double *_C = buf.data() + 0;
   Strassen<16>(n, A, B, _C);
   matrixAdd(n, C, _C, C);
 }
