64285d859ad64c41cc05dc144191c8257c16954e
MatveevDaniil <dmatveev@sfsu.edu>
2024-09-23 18:22:44 -0700

    FIX: strassen limits for 64 and 256

--- dgemm-strassen/dgemm-strassen-64.cpp
+++ dgemm-strassen/dgemm-strassen-64.cpp
@@ -5,6 +5,6 @@ void square_dgemm(int n, double* A, double* B, double* C)
 {
   std::vector<double> buf(n * n);
   double *_C = buf.data() + 0;
-  Strassen<16>(n, A, B, _C);
+  Strassen<64>(n, A, B, _C);
   matrixAdd(n, C, _C, C);
 }
